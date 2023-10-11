3b487dbf921d0b95ee0aaea61d089f836774d0de
TerrenceChao <rtyufjvbn@gmail.com>
2023-10-11 15:49:47 +0800

    add 'meta' validator

--- src/configs/constants.py
+++ src/configs/constants.py
@@ -1,5 +1,27 @@
 from enum import Enum
 
+REGION_MAPPING = {
+    'us-east-1': 'us-e1',
+    'us-east-2': 'us-e2',
+    'us-west-1': 'us-w1',
+    'us-west-2': 'us-w2',
+    'ca-central-1': 'ca-c1',
+    'eu-north-1': 'eu',
+    'eu-west-2': 'uk',
+    'eu-west-3': 'fr',
+    'eu-south-1': 'it',
+    'eu-central-1': 'de',
+    'ap-northeast-1': 'jp',
+    'ap-northeast-2': 'kr',
+    'ap-southeast-1': 'sg',
+    'ap-southeast-2': 'au',
+    'ap-south-1': 'in',
+    'sa-east-1': 'br'
+}
+
+
+VALID_ROLES = set(['company', 'teacher'])
+
 
 class Apply(Enum):
     PENDING = 'pending'
--- src/domains/user/value_objects/auth_vo.py
+++ src/domains/user/value_objects/auth_vo.py
@@ -1,9 +1,37 @@
-from pydantic import BaseModel, EmailStr
+import json
+from pydantic import BaseModel, EmailStr, validator
+from ....configs.constants import VALID_ROLES, REGION_MAPPING
+from ....configs.exceptions import *
+
+REGION_CODES = set(REGION_MAPPING.values())
+
+def meta_validator(meta: str):
+    try:
+        meta_json = json.loads(meta)
+        if not meta_json['role'] in VALID_ROLES:
+            raise ClientException(msg=f'role allowed only in {VALID_ROLES}')
+        
+        if not meta_json['region'] in REGION_CODES:
+            raise ClientException(msg=f'region is not allowed')
+
+        return meta
+
+    except json.JSONDecodeError as e:
+        log.error(
+            f'func: meta_validator error [json_decode_error] meta:%s, err:%s', meta, e.__str__())
+        raise ClientException(msg=f'invalid json format, meta:{meta}')
+
+    except ClientException as e:
+        raise ClientException(msg=e.msg)
 
 
 class SignupVO(BaseModel):
     email: EmailStr
     meta: str
+    
+    @validator('meta')
+    def check_meta(cls, v):
+        return meta_validator(v)
     
     class Config:
         schema_extra = {
