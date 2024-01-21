d853e82c32415e9e382e7e357ae59fbe3217e036
TerrenceChao <rtyufjvbn@gmail.com>
2024-01-21 17:32:06 +0800

    improve the payment process

--- src/domains/payment/models/value_objects.py
+++ src/domains/payment/models/value_objects.py
@@ -1,4 +1,4 @@
-from typing import Optional, Dict, Any
+from typing import Optional
 from pydantic import BaseModel
 from .models import *
 from ..configs.constants import *
@@ -8,15 +8,6 @@ from ....infra.utils.time_util import *
 class PaymentStatusVO(BaseModel):
     role_id: int
     status: PaymentStatusEnum
+    subscribe_status: SubscribeStatusEnum
     current_period_end: Optional[int]
     valid: bool = False
-
-    def __init__(self, **data):
-        super().__init__(**data)
-        if data.get('current_period_end', -1) > 0:
-            self.valid = \
-                self.status in PAYMENT_PERIOD or \
-                self.current_period_end > current_seconds()
-
-        elif data.get('payout_id') != None:
-            self.valid = True
