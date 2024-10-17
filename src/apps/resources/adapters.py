5367067ab77e9dba8ca917c5d602f98658c2f3ff
terrencechao <TerrenceChao@users.noreply.github.com>
2024-10-17 17:52:50 +0800

    format style

--- src/apps/resources/adapters.py
+++ src/apps/resources/adapters.py
@@ -1,5 +1,6 @@
 from .manager import io_resource_manager
 from .handlers.http_resource import HttpResourceHandler
+from .handlers.cache_resource import DynamodbCacheResourceHandler
 from ...infra.client.service_api_dapter import ServiceApiAdapter
 from ...infra.cache.dynamodb_cache_adapter import DynamoDbCacheAdapter
 
@@ -8,5 +9,5 @@ http_resource_handler: HttpResourceHandler = io_resource_manager.get('http')
 service_client = ServiceApiAdapter(http_resource_handler)
 
 # cache
-cache_resource_handler = io_resource_manager.get('ddb_cache')
+cache_resource_handler: DynamodbCacheResourceHandler = io_resource_manager.get('ddb_cache')
 gw_cache = DynamoDbCacheAdapter(cache_resource_handler)
